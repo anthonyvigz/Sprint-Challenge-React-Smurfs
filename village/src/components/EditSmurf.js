e71ab619db1f08de17e12c317cd084cb737821aa
anthonyvigz <anthonyvigz@gmail.com>
2019-06-30 19:44:10 -0400

    created edit smurf component, will house delete and edit

---
+++ village/src/components/EditSmurf.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class EditSmurf extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      height: ''
+    };
+  }
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+
+    const { name, age, height } = this.state;
+    const payload = { name, age, height }
+
+    axios.post("http://localhost:3333/smurfs", payload)
+      .then((response) => {
+
+        this.props.updateSmurfs(response.data);
+
+        console.log(response.data)
+      })
+      .catch((err) => {
+        console.log("Error:", err)
+      })
+
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="SmurfForm">
+        <form onSubmit={this.addSmurf}>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="name"
+            value={this.state.name}
+            name="name"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="age"
+            value={this.state.age}
+            name="age"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="height"
+            value={this.state.height}
+            name="height"
+          />
+          <button type="submit">Add to the village</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EditSmurf;
